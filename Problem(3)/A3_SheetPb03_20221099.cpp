d801a69b8b5b11e9d7d6c581dcc1f9873d749151
Abdulrahman <test@test.com>
2023-12-07 15:28:09 +0200

    Uploading pb3-6

---
+++ Problem(3)/A3_SheetPb03_20221099.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Returns True if it is a punc excluding the '-'
+bool is_punc(char c) {
+    return ispunct(c) && c != '-';
+}
+//Returns true if it is alpha numeric including '-'
+bool is_alphanumeric(char c) {
+    return isalnum(c) || c == '-';
+}
+
+void toLowerCase(string& str) {
+    for (char& c : str) {
+        c = tolower(c);
+    }
+}
+
+void createFrequencyTable(string fileName) {
+    map<string, int> frequencyTable;
+    string word;
+
+    ifstream file(fileName);
+
+    while (file >> word) {
+        // Clean the word of punc and non alphanumeric characters
+        string cleanWord;
+        for (char c : word) {
+            if (is_alphanumeric(c) and !is_punc(c)) {
+                cleanWord += c;
+            }
+        }
+
+        toLowerCase(cleanWord);
+
+        if (!cleanWord.empty()) {
+            frequencyTable[cleanWord]++;
+        }
+    }
+
+    file.close();
+
+    //Output
+    for (pair<string, int> pair : frequencyTable) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+}
+
+int main() {
+    string fileName;
+
+    cout << "enter the file name: ";
+    cin >> fileName;
+
+    createFrequencyTable(fileName);
+
+}
---
+++ Problem(6)/A3_SheetPb06_20221099.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+class Universe{
+    int temp[10][10];
+    int arr[10][10];
+    
+public:
+    //Generates Starting layouts
+    void intialize(int i, int j) {
+        arr[i][j] = 1;
+    }
+//Sets all cells to zero
+    void reset() {
+        for (int i = 0; i <10; i++)
+            for(int j = 0; j <10; j++) {
+                arr[i][j] = 0;
+            }
+    }
+    //return all number of alive cells
+    int count_alive() {
+        int count{0};
+        for (int i = 0; i <10; i++)
+            for(int j = 0; j <10; j++) {
+                if (arr[i][j] == 1)
+                    count ++;
+            }
+        return count;
+    }
+
+bool is_valid(int x, int y) const{
+    return x>0 && y>0 && x<10 && y<10;
+}
+    int count_neighbours(int cordx, int cordy) {
+
+        int dx[] = {0, -1, 0, 1, 1, 1, -1, -1};
+        int dy[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+        int cnt =0;
+        for (int k = 0  ;k<8 ; k++) {
+            int currx = cordx + dx[k], curry = cordy + dy[k];
+            if(is_valid(currx,curry) && arr[currx][curry]){
+                cnt++;
+            }
+        }
+        return cnt;
+
+    }
+
+    //Produces the state of the next generation
+    void next_generation() {
+                for (int  i = 0; i < 10; i++) {
+                    for (int j = 0 ; j < 10; j++) {
+                        temp[i][j] = count_neighbours(i,j);
+                    }
+                }
+
+                for (int i = 0; i < 10; i++) {
+                    for(int j = 0; j < 10; j++) {
+                        if (temp[i][j] == 3)    arr[i][j] = 1;
+                        else if (temp[i][j] > 3) arr[i][j] = 0;
+                        else if (temp[i][j] < 2) arr[i][j] = 0;
+                    }
+                }
+
+            }
+
+    //Shows the game status on the console after clearing the screen.
+    void display() {
+        cout << endl << endl << endl;
+        for (int i = 0; i  < 10; i++) {
+            for (int j = 0; j < 10;j++) {
+                cout << arr[i][j] << ' ';
+            }
+            cout << endl;
+        }
+
+    }
+    //Start the game for certain number of truns.
+    void run () {
+
+       //Clear the Unviverse
+       reset();
+
+       //Initialize the alive cells;
+       cout << "Enter the number of cells you want to make alive:  " << endl;
+       int alive_cells;
+       cin >> alive_cells;
+       while (alive_cells--) {
+           cout << "Enter the i, j coordinates of the cell: " << endl;
+           int i, j;
+           cin >> i >> j;
+           intialize(i,j);
+       }
+//show Initial condition
+       display();
+
+    cout << "Enter number of turns" << endl;
+    int turns; cin >> turns;
+
+    while (turns--) {
+        next_generation();
+        display();
+        cout << endl << endl;
+        Sleep(2000);
+    }
+
+
+
+
+    }
+};
+
+
+
+int main () {
+    Universe u;
+    u.run();
+
+}
